5cb62ecaf229806f7b72766a4b7ebd33b4b79db0
Devon Peet <devondpeet@gmail.com>
2020-07-24 15:36:22 -0400

    removing comments

--- src/App.js
+++ src/App.js
@@ -5,20 +5,6 @@ import clinical from "./Clinical-Particulars-Tab-onSelect.svg"
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <div className="icon1">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
           <g fill="none" fill-rule="evenodd">
